be101a450139ba4fa0089c5afb57ec0be70e560e
ModenaRose <144450309+ModenaRose@users.noreply.github.com>
2023-11-10 14:23:03 -0300

    Initial commit

---
+++ Assets/MusicPause.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPause : MonoBehaviour
+{
+      public AudioSource MusicPlayer;
+
+      private void OnTriggerEnter(Collider other)
+        {
+
+
+            //Debug.Log(other.gameObject.name);
+
+            MusicPlayer.Pause();
+
+
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+
+            //Debug.Log(other.gameObject.name);
+
+            MusicPlayer.Play();
+
+
+        }
+
+
+
+
+
+
+}
---
+++ Assets/TriggerPlaySFX.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TriggerPlaySFX : MonoBehaviour
+{
+	public AudioSource SFX;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		
+		SFX.Play();
+
+	}
+
+}
